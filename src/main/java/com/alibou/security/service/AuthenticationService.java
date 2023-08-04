c972303e0b0420ce453b36bdb71732ac699e2392
OdogwuLegends <odigbocharlesjnrlegends@gmail.com>
2023-08-04 19:12:26 +0100

    completed the authenticate method

--- src/main/java/com/alibou/security/service/AuthenticationService.java
+++ src/main/java/com/alibou/security/service/AuthenticationService.java
@@ -8,6 +8,8 @@ import com.alibou.security.enums.Role;
 import com.alibou.security.model.User;
 import com.alibou.security.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +21,8 @@ public class AuthenticationService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+
+    private final AuthenticationManager authenticationManager;
 
     public AuthenticationResponse register(RegistrationRequest request) {
         var user = User.builder()
@@ -37,6 +41,13 @@ public class AuthenticationService {
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
-        
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(),request.getPassword()));
+        var user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 }
