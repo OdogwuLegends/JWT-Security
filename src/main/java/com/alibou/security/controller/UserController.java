74c2a921e326a5650aa59840e93bc5325617a3af
OdogwuLegends <odigbocharlesjnrlegends@gmail.com>
2023-08-04 18:45:52 +0100

    set up user controller and add two end points

---
+++ src/main/java/com/alibou/security/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.alibou.security.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/auth")
+public class UserController {
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegistrationRequest request){
+
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody AuthenticationRequest request){
+
+    }
+}
