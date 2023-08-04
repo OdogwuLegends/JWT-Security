82fdca15a319dbd4c5e98a573db01f2ac05db3f6
OdogwuLegends <odigbocharlesjnrlegends@gmail.com>
2023-08-04 17:13:56 +0100

    created secret key for signing in

--- src/main/java/com/alibou/security/service/JwtService.java
+++ src/main/java/com/alibou/security/service/JwtService.java
@@ -1,10 +1,31 @@
 package com.alibou.security.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+    private static final String SECRET_KEY = "4f951911d14044651adc60a4ab8fd8e8a32d4bf9e3d4516170c6b1d4cbe0f9d7";
     public String extractUsername(String jwtToken) {
         return null;
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
