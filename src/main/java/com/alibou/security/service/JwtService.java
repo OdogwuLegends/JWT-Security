866cf72928f773f8fc9b2a3296e99b26a59d6fcb
OdogwuLegends <odigbocharlesjnrlegends@gmail.com>
2023-08-04 17:21:51 +0100

    created methods to extract single claim and all claims in the Jwt service

--- src/main/java/com/alibou/security/service/JwtService.java
+++ src/main/java/com/alibou/security/service/JwtService.java
@@ -7,12 +7,17 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
     private static final String SECRET_KEY = "4f951911d14044651adc60a4ab8fd8e8a32d4bf9e3d4516170c6b1d4cbe0f9d7";
     public String extractUsername(String jwtToken) {
         return null;
+    }
+    public <T> T extractClaim(String token, Function<Claims,T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
     }
 
     private Claims extractAllClaims(String token){
