bcecfadc6fc6987f2453b652e9a7cb540f41b61a
OdogwuLegends <odigbocharlesjnrlegends@gmail.com>
2023-08-04 15:55:59 +0100

    created an entity user

---
+++ src/main/java/com/alibou/security/model/User.java
@@ -0,0 +1,25 @@
+package com.alibou.security.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "App_User")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
