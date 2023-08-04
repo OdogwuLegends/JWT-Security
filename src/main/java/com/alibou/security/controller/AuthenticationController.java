dc0d320f2d867fb1fffe16e5f94e1eb7ab02dd63
OdogwuLegends <odigbocharlesjnrlegends@gmail.com>
2023-08-04 19:17:06 +0100

    added a demo controller

--- src/main/java/com/alibou/security/controller/AuthenticationController.java
+++ src/main/java/com/alibou/security/controller/AuthenticationController.java
@@ -14,8 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
-public class UserController {
-
+public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @PostMapping("/register")
