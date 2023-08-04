7eafe6bb0be674351fae5cde02da5669cae7f58c
OdogwuLegends <odigbocharlesjnrlegends@gmail.com>
2023-08-04 19:08:00 +0100

    completed the registration method in the user service

--- src/main/java/com/alibou/security/service/AuthenticationService.java
+++ src/main/java/com/alibou/security/service/AuthenticationService.java
@@ -1,19 +1,42 @@
 package com.alibou.security.service;
 
+import com.alibou.security.config.JwtService;
 import com.alibou.security.dto.request.AuthenticationRequest;
 import com.alibou.security.dto.request.RegistrationRequest;
 import com.alibou.security.dto.response.AuthenticationResponse;
+import com.alibou.security.enums.Role;
+import com.alibou.security.model.User;
+import com.alibou.security.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import static com.alibou.security.enums.Role.USER;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
     public AuthenticationResponse register(RegistrationRequest request) {
-        
+        var user = User.builder()
+                .firstName(request.getFirstname())
+                .lastName(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(USER)
+                .build();
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        
     }
 }
